419bf7b771a3bb410b6c64edc968e6e5f563db3d
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-12-24 14:37:14 +0530

    Initial Commit

---
+++ dicee-ui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  dicee-ui
+//
+//  Created by Saurabh Mirajkar on 24/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dicee-ui/dicee_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicee_uiApp.swift
+//  dicee-ui
+//
+//  Created by Saurabh Mirajkar on 24/12/23.
+//
+
+import SwiftUI
+
+@main
+struct dicee_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
