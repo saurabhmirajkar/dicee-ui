7fcdaeadcb760b5a5f7325ba540df64be6f23182
Saurabh Mirajkar <mirajkar46@gmail.com>
2023-12-24 15:09:31 +0530

    ui complete

--- dicee-ui/ContentView.swift
+++ dicee-ui/ContentView.swift
@@ -9,13 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("background")
-            .resizable()
-            .aspectRatio(contentMode: .fill)
-            .ignoresSafeArea()
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea(.all)
+
+            
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(n: 1)
+                    DiceView(n: 1)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    // onclick action
+                }, label: {
+                    Text("Roll")
+                        .font(.system(size:50))
+                        .fontWeight(.heavy)
+                        .foregroundStyle(.white)
+                        .padding(.horizontal)
+                })
+                .background(.red)
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DiceView: View {
+    let n: Int
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding(.all)
+    }
 }
